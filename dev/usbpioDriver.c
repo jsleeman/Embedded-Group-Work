cd75652940ab8fab263fcb4af3ee14c47f6297f7
Sleeman J.C. CEMS BSc/CompSysInt 1 <jc2-sleeman@agneslinux.netlab.cems.uwe.ac.uk>
2012-05-16 21:42:49 +0100

    fixing naming issues and pointer problems more to go

--- dev/usbpioDriver.c
+++ dev/usbpioDriver.c
@@ -2,6 +2,7 @@
 #include <linux/kernel.h> /* Needed for KERN_ALERT */
 #include <linux/slab.h>   /* Needed for kmalloc  */
 #include <linux/usb.h>    /* Needed for USB functions */
+#include <asm/uaccess.h>  /* Needed for copy_from_user */
 #include "usbPioDriver.h"
 
 MODULE_LICENSE("GPL");
@@ -99,7 +100,7 @@ static int usbPioRelsease(struct inode *inode, struct file *file)
 
 /* Sources: Essential Linux Device Drivers 
             http://www.cems.uwe.ac.uk/~cduffy/dwd/usbpio.pdf
-	    http://www.cems.uwe.ac.uk/~cduffy/lkmpg.pdf
+	        http://www.cems.uwe.ac.uk/~cduffy/lkmpg.pdf
 */
 
 /* Kernel return error codes: ENOMEM = Used when no mem available
@@ -109,155 +110,155 @@ static int usbPioRelsease(struct inode *inode, struct file *file)
 /* Pipes -> interger encoding of: endpoint address, direction (in /out), type of data transfer(control, bulk, irq, bulk, isochronous) */
 
 static void usbPioDisconnect(struct usb_interface *interface)
-  {
-    printk("Starting usbPioDisconnect\n");
-    usbPiodeviceT *usbPiodevice;
+{
+  printk("Starting usbPioDisconnect\n");
+  usbPioDeviceT *usbPioDevice;
     
-    /*Retrieve data saved with usb_set_intfdata - Free memory*/
-    usbPiodevice = usb_get_intfdata(interface); //TODO Free memory
+  /*Retrieve data saved with usb_set_intfdata - Free memory*/
+  usbPioDevice = usb_get_intfdata(interface); //TODO Free memory
     
-    /*Zero out interface data*/
-    usb_set_intfdata(interface, NULL);
+  /*Zero out interface data*/
+  usb_set_intfdata(interface, NULL);
     
-    /* release /dev/usbPio */
-    usb_deregister_dev(interface, &usbPioClass);
+  /* release /dev/usbPio */
+  usb_deregister_dev(interface, &usbPioClass);
     
-    usbPiodevice->interface = NULL;
-  }
+  usbPioDevice->interface = NULL;
+}
 
 static int usbPioOpen(struct inode *inode, struct file *file)
-  {
-    printk("Starting UsbPioOpen");
+{
+  printk("Starting UsbPioOpen");
 
-    usbPioDeviceT *usbPioDevice;
-    struct usb_interface *interface;
+  usbPioDeviceT *usbPioDevice;
+  struct usb_interface *interface;
 
-    /* Get the interface the device is associated with */
-    interface = usb_find_interface(&usbPioDriver, iminor(inode));
-    if (!interface)
-      {
-	printk (KERN_ALERT "Error: Can't find device for minor %d\n",__FUNCTION__, iminor(inode));
-	return -ENODEV;
-      }
+  /* Get the interface the device is associated with */
+  interface = usb_find_interface(&usbPioKeypadDriver, iminor(inode));
+  if (!interface)
+    {
+      printk (KERN_ALERT "Error: Can't find device for minor %d\n", iminor(inode));
+      return -ENODEV;
+    }
 
-    /* This data was saved in usbPioProbe*/
-    usbPioDevice = usb_get_intfdata(interface);
+  /* This data was saved in usbPioProbe*/
+  usbPioDevice = usb_get_intfdata(interface);
     
-    if()
-      {
-	printk (KERN_ALERT "usb_get_intfdata failed\n");
-	return -ENODEV;
-      }
+  if(usbPioDevice)
+    {
+      printk (KERN_ALERT "usb_get_intfdata failed\n");
+      return -ENODEV;
+    }
 
-    /* Saves the device specific object for use with read and write*/
-    file->private_data = usbPioDevice;
-  }
+  /* Saves the device specific object for use with read and write*/
+  file->privatDeviceT *usbPioDevice;
+}
 
 static ssize_t usbPioRead(struct file *file, char __user *buffer, size_t count, loff_t *ppos)
-  {
-    printk("starting usbPioRead\n");
+{
+  printk("starting usbPioRead\n");
     
-    int retval;
-    usbPioDeviceT *usbPioDevice;
-    struct urb *urb;
-    char *dma_buffer;
+  int retval;
+  usbPioDeviceT *usbPioDevice;
+  struct urb *urb;
+  char *dma_buffer;
     
-    /* Get the address of the device, data saved in open */
-    usbPioDevice = (usbPioDeviceT *)file->private_data;
+  /* Get the address of the device, data saved in open */
+  usbPioDevice = (usbPioDeviceT *)file->private_data;
 
-    /* Allocate some mem for the URB*/
-    usb = urb_alloc_urb(0, GFP_KERNEL);
-    if (!usb)
-      {
-	printk(KERN_ALERT "Memory allocation for the URB failed\n");
-	return -ENOMEM;
-      }
+  /* Allocate some mem for the URB*/
+  urb = urb_alloc_urb(0, GFP_KERNEL);
+  if (!urb)
+    {
+      printk(KERN_ALERT "Memory allocation for the URB failed\n");
+      return -ENOMEM;
+    }
     
-    dma_buffer = usb_alloc_coherent(usbPioDevice->usbDev, usbPioDevice->inBulkBufferSize, GFP_KERNEL, &urb->transfer_dma);
-    if (!dma_buffer)
-      {
-	printk(KERN_ALERT "Error: failed to allocate mem for usb dma buffer\n");
-	return -ENOMEM;
-      }
+  dma_buffer = usb_alloc_coherent(usbPioDevice->usbDev, usbPioDevice->inBulkBufferSize, GFP_KERNEL, &urb->transfer_dma);
+  if (!dma_buffer)
+    {
+      printk(KERN_ALERT "Error: failed to allocate mem for usb dma buffer\n");
+      return -ENOMEM;
+    }
 
-    /*Init the urb*/
-    usb_fill_bulk_urb(urb,
-		      usbPioDevice->usbDev,
-		      dma_buffer,
-		      count,
-		      usbPioReadCallBack,
-		      usbPioDevice);
+  /*Init the urb*/
+  usb_fill_bulk_urb(urb,
+		    usbPioDevice->usbDev,
+		    dma_buffer,
+		    count,
+		    usbPioReadCallBack,
+		    usbPioDevice);
 
-    retval = usb_submit_urb(usbPioDevice->ctrl_urb, GFP_ATOMIC);
-    if (retval)
-      {
-	printk(KERN_ALERT "Error: failed to submit the URB, error number: %d", retval);
-	return-EFAULT;
-      }
+  retval = usb_submit_urb(usbPioDevice->ctrl_urb, GFP_ATOMIC);
+  if (retval)
+    {
+      printk(KERN_ALERT "Error: failed to submit the URB, error number: %d", retval);
+      return-EFAULT;
+    }
    
-    return count;
-  }
+  return count;
+}
 
 static ssize_t usbPioWrite(struct file *file, const char __user *user_buffer, size_t count, loff_t *ppos)
-  {
-    printk("Starting usbPioWrite");
+{
+  printk("Starting usbPioWrite");
     
-    size_t bufferSize = count;
-    char *dmaBuffer; /*used for usb_buffer_alloc*/
-    struct urb *urb;
-    struct usbPioDeviceT *usbPioDevice;
-    int retval;
+  size_t bufferSize = count;
+  char *dmaBuffer; /*used for usb_buffer_alloc*/
+  struct urb *urb;
+  struct usbPioDeviceT *usbPioDevice;
+  int retval;
 
-    /* Get the address of the device, data saved in open */
-    usbPioDevice = (usbPioDeviceT *)file->private_data;
+  /* Get the address of the device, data saved in open */
+  usbPioDevice = (usbPioDeviceT *)file->private_data;
 
-    /* Allocate some mem for the URB*/
-    usb = urb_alloc_urb(0, GFP_KERNEL);
-    if (!usb)
-      {
-	printk(KERN_ALERT "Memory allocation for the URB failed\n");
-	return -ENOMEM;
-      }
+  /* Allocate some mem for the URB*/
+  urb = urb_alloc_urb(0, GFP_KERNEL);
+  if (!urb)
+    {
+      printk(KERN_ALERT "Memory allocation for the URB failed\n");
+      return -ENOMEM;
+    }
     
-    /* sets up the dma*/
-    dmaBuffer = usb_alloc_coherent(usbPioDevice->usbDev, usbPioDevice->inBulkBufferSize, GFP_KERNEL, &urb->transfer_dma);
-    if (!dmaBuffer)
-      {
-	printk(KERN_ALERT "Error: failed to allocate mem for usb dma buffer\n");
-	return -ENOMEM;
-      }
+  /* sets up the dma*/
+  dmaBuffer = usb_alloc_coherent(usbPioDevice->usbDev, usbPioDevice->inBulkBufferSize, GFP_KERNEL, &urb->transfer_dma);
+  if (!dmaBuffer)
+    {
+      printk(KERN_ALERT "Error: failed to allocate mem for usb dma buffer\n");
+      return -ENOMEM;
+    }
     
-    /* Copies the data from userland to kernel land */
-    if(copy_from_user(dmaBuffer, user_buffer, bufferSize))
-      {
-	printk(KERN_ALERT"Error: Failed to copy data from user land to the DMA buffer, error number: %d", retval);
-	return-EFAULT;
-      }
+  /* Copies the data from userland to kernel land */
+  if(copy_from_user(dmaBuffer, user_buffer, bufferSize))
+    {
+      printk(KERN_ALERT"Error: Failed to copy data from user land to the DMA buffer, error number: %d", retval);
+      return-EFAULT;
+    }
     
-    retval = usb_submit_urb(urb, GFP_KERNEL);
-    if(retval)
-      {
-	printk(KERN_ALERT "usb_submit_urb, error: %d", retval);
-	return-EFAULT;
-      }
+  retval = usb_submit_urb(urb, GFP_KERNEL);
+  if(retval)
+    {
+      printk(KERN_ALERT "usb_submit_urb, error: %d", retval);
+      return-EFAULT;
+    }
 
-    return bufferSize;
-  }
+  return bufferSize;
+}
 static void usbPioWriteCallBack(struct urb *urb)
 {
   /* urb->status contains the submission status. 0 if seccessful, ignore the 3 errors in the if below, dunno why the good book says too */
   if (urb->status && !(urb->status == -ENOENT || urb->status == -ECONNRESET || urb->status == -ESHUTDOWN))
     {
-      printk(KERN_ALERT "Error: URB Status:%s",urb->status);
+      printk(KERN_ALERT "Error: URB Status:%d",urb->status);
     }
   /* free up our allocated buffer */
   usb_free_coherent(urb->dev, urb->transfer_buffer_length,urb->transfer_buffer, urb->transfer_dma);
 }
 
-static void usbOioKeypadReadCallBack(struct urb *urb)
+static void usbPioReadCallBack(struct urb *urb)
 {
   char *buf;
-  struct usbPioDeviceT *usbPioDevice;
+  usbPioDeviceT *usbPioDevice;
 
   usbPioDevice = (struct usbPioDeviceT*)urb->context;
 
@@ -266,15 +267,15 @@ static void usbOioKeypadReadCallBack(struct urb *urb)
 
   /* urb->status contains the submission status. 0 if seccessful, ignore the 3 errors in the if below, dunno why the good book says too */
   if (urb->status && !(urb->status == -ENOENT || urb->status == -ECONNRESET || urb->status == -ESHUTDOWN))
-  {
-           printk(KERN_ALERT "Error: URB Status:%s",urb->status);
-  }
+    {
+      printk(KERN_ALERT "Error: URB Status:%d",urb->status);
+    }
 
   /* Copy to userspace */
   copy_to_user(usbPioDevice->user_buffer, buf, urb->actual_length);
  
   /* free up our allocated buffer */
   usb_free_coherent(urb->dev, urb->transfer_buffer_length,
-  urb->transfer_buffer, urb->transfer_dma);
+		    urb->transfer_buffer, urb->transfer_dma);
 }
 
